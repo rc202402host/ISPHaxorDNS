cc041d1c0432b47d64c6d3e93aaa0b08c3345cf0
rc202402host <48879354+rc202402host@users.noreply.github.com>
2019-03-24 10:09:18 +0000

    run.py
    Python3 Run Script with ENV Vars

--- run.py
+++ run.py
@@ -2,9 +2,14 @@
 
 import os
 
+host_ip = os.getenv('HOST_IP')
+host_password = os.getenv('HOST_PASSWORD')
+host_domain = os.getenv('HOST_DOMAIN')
 
 print("Staring iodine bot...")
 
-resp = os.system("iodined -c -f 10.0.0.1 -P isphaxordns isphaxordns.herokuapp.com")
+# iodined -c -f 10.0.0.1 -P yourpasswordhere iodine.yourdomain.com
+
+resp = os.system("iodined -c -f " + host_ip " -P " + host_password " " + ihost_domain )
 
 print("Run Response: " + str(resp))
